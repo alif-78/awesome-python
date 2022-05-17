70a8157374d28c1e56e940578064ee9fe481768e
alif-78 <bhuiyan1807078@stud.kuet.ac.bd>
2022-05-18 01:24:34 +0600

    lab is added

---
+++ lab.java
@@ -0,0 +1,18 @@
+public class Person {
+Person{
+}
+Person(string name)
+{
+   this.name = name
+
+}
+  public String name; 
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+}
