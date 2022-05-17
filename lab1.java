bea8e1e066f1251e1a9c50bcaebf274724870037
alif-78 <bhuiyan1807078@stud.kuet.ac.bd>
2022-05-18 01:57:31 +0600

    br1 java file is commited

---
+++ lab1.java
@@ -0,0 +1,20 @@
+public class student {
+student()
+{
+  
+}
+student(String name)
+{
+   this.name = name
+
+}
+  public String name; 
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+}
